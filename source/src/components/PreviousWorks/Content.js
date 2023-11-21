aae28c477628a143468ad776ad589258d732b73e
StarMastar1126 <starmastar1126@gmail.com>
2023-11-21 04:03:10 -0800

    change structure

--- source/src/components/PreviousWorks/Content.js
+++ source/src/components/PreviousWorks/Content.js
@@ -10,7 +10,7 @@ const Content = ({ data }) => {
                 <div className='item'>
                     <div className='company-info'>
                         <div className='company-name'>{item.company}</div>
-                        {item.logo && <img alt='logo' src={`${process.env.PUBLIC_URL}/assets/images/${item.logo}`} />}
+                        {item.logo && <img alt='logo' src={`${process.env.PUBLIC_URL}/assets/icons/company/${item.logo}`} />}
                         <div className='company-date'>{item.date}</div>
                         <div className='company-status'>{item.status}</div>
                     </div>
@@ -22,7 +22,7 @@ const Content = ({ data }) => {
                             <div className='work-type' dangerouslySetInnerHTML={{ __html: item.type }} />
                             <div className='work-client'>{`- Client`}</div>
                             <div className='work-avatar-container'>
-                                {item.avatar && <img alt='avatar' className='work-avatar' src={`${process.env.PUBLIC_URL}/assets/images/${item.avatar}`} />}
+                                {item.avatar && <img alt='avatar' className='work-avatar' src={`${process.env.PUBLIC_URL}/assets/icons/avatar/${item.avatar}`} />}
                                 <div className='client-content' dangerouslySetInnerHTML={{ __html: item.client }} />
                             </div>
 
@@ -38,7 +38,7 @@ const Content = ({ data }) => {
                             <div className='work-icons'>
                                 {
                                     item.icons?.map((src, index) => (
-                                        <img key={index} alt='skill-icon' src={`${process.env.PUBLIC_URL}/assets/icons/${src}`} />
+                                        <img key={index} alt='skill-icon' src={`${process.env.PUBLIC_URL}/assets/icons/software/${src}`} />
                                     ))
                                 }
                             </div>
@@ -46,10 +46,10 @@ const Content = ({ data }) => {
                             <img className='work-banner' src={`${process.env.PUBLIC_URL}/assets/images/banner.png`} />
                             <ImageSlider images={item.images} />
                             <div className='button-group'>
-                                {item.appstore !== '' && <button className='button' onClick={() => window.open(item.appstore, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/images/appstore.png`} /></button>}
-                                {item.playstore !== '' && <button className='button' onClick={() => window.open(item.playstore, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/images/playstore.png`} /></button>}
-                                {item.youtube !== '' && <button className='button' onClick={() => window.open(item.youtube, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/images/youtube.png`} /></button>}
-                                {item.website !== '' && <button className='button' onClick={() => window.open(item.website, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/images/website.png`} /></button>}
+                                {item.appstore !== '' && <button className='button' onClick={() => window.open(item.appstore, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/icons/software/appstore.png`} /></button>}
+                                {item.playstore !== '' && <button className='button' onClick={() => window.open(item.playstore, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/icons/software/playstore.png`} /></button>}
+                                {item.youtube !== '' && <button className='button' onClick={() => window.open(item.youtube, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/icons/software/youtube.png`} /></button>}
+                                {item.website !== '' && <button className='button' onClick={() => window.open(item.website, '_blank')}><img className='button-icon' src={`${process.env.PUBLIC_URL}/assets/icons/software/website.png`} /></button>}
                             </div>
                             <div className='work-other' dangerouslySetInnerHTML={{ __html: item.other }} />
                         </div >
